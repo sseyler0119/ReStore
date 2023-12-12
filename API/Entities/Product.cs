a2aeeaa9d5bafc0b4406f4c1bc15fe84198840a0
Serena Seyler <sseyler0119@gmail.com>
2023-12-12 13:49:40 -0800

    initial commit

---
+++ API/Entities/Product.cs
@@ -0,0 +1,14 @@
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; } // we're using long vs decimal because of the payment processor we are goign to use
+        public string PictureUrl { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,44 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. order does not matter for services
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+/* order matters with middleware */
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+/* create database and seed data */
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try
+{
+    context.Database.Migrate(); //apply pending migrations, create db if does not already exist
+    DbInitializer.Initialize(context); // use Initializer method we created in DbInitializer class, pass in context
+}
+catch (Exception ex)
+{   
+    logger.LogError(ex, "A Problem occurred during migration");
+}
+
+app.Run();
